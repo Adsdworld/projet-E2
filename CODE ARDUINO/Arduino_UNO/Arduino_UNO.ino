ac28a8fe940aff6c8cbe6e11587f632abe8408db
Adsdworld <128044117+Adsdworld@users.noreply.github.com>
2023-10-23 17:10:59 +0200

    début de réadaction du code

---
+++ CODE ARDUINO/Arduino_UNO/Arduino_UNO.ino
@@ -0,0 +1,139 @@
+int TimeoutMsgResponse=1000;
+int TimeoutArduinoUno=20000;
+
+String ReadInstantMsgFromArduino(){
+  String StringBuilder="";
+  while(true){
+    if (0<Serial.available()){
+      delay(TimeoutMsgResponse); //laisser un peu de temps pour que le court message arrive
+      while(Serial.available()>0){
+        char Serialdata =Serial.read();
+        StringBuilder+=String(Serialdata);
+      }
+      return StringBuilder;
+    }
+  }
+  return "NO MESSAGE FOUND";
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  if (0<Serial.available()){
+    String StringBuilder="";
+    delay(TimeoutMsgResponse); //laisser un peu de temps pour que le court message arrive
+    while(Serial.available()>0){
+      char Serialdata =Serial.read();
+      StringBuilder+=String(Serialdata);
+    }
+    if (StringBuilder==String("START")){//affiche une animation cool de démarrage
+      Serial.print("OK");
+      Start();
+      Serial.print("STARTED");
+    }
+    else if(StringBuilder==String("CONNECTING")){
+      Serial.print("OK");
+      Connecting();
+    }
+    else if (StringBuilder==String("WELCOME")){//affiche une animation cool de démarrage
+      Serial.print("OK");
+      Welcome();
+      Serial.print("TOUCH");
+    }
+    else if(StringBuilder==String("RFID")){
+      Serial.print("OK");
+      RFID();
+    }
+    else if(StringBuilder.charAt(0)=='D'){
+      Serial.print("OK");
+      DigiCode(StringBuilder);
+    }
+    else if(StringBuilder==String("DATABASE")){
+      Serial.print("OK");
+      DataBase();
+    }
+    else{
+      Serial.print("ERROR");
+    }
+  }
+}
+
+
+String GetCardID(){ //exemple test pour le RFID
+  delay(1000);
+  Shiny(100, 50);
+  return "D2HJ82";
+}
+void Shiny(int nb, int ms){
+  for (int i=1;i<nb;i++){
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(ms);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(ms);
+  }
+  return;
+}
+
+void Start(){
+  int Timeout=millis()+TimeoutArduinoUno;
+  while(true){
+    while(millis()<Timeout){
+      //Some screen animations, do not exceed TimeoutArduinoUno or découper en petite animations et vérifier le timetout entre chaques pour svoir si oui ou non envoyer un KeepAlive
+      return;
+    }
+    Serial.print("KA");
+  }
+}
+void Connecting(){
+  while(true){
+    //3 fast wifi screen animation
+    if (ReadInstantMsgFromArduino()=="CONNECTED"){
+      return;
+    }
+  }
+}
+void Welcome(){
+  int Timeout=millis()+TimeoutArduinoUno;
+  while(true){
+    while(millis()<Timeout){
+      //if (screen is touch){return;}
+    }
+    Serial.print("KA");
+  }
+}
+void RFID(){
+  while(true){
+    //animations courtes?
+    if (ReadInstantMsgFromArduino()=="RFIDED"){
+      return;
+    }
+  }
+}
+void DigiCode(String Message){
+  if (Message=="D0"){
+    //_ _ _ _
+  }
+  else if (Message=="D1"){
+   //* _ _ _   
+  }
+  else if (Message=="D2"){
+    // * * _ _
+  }
+  else if (Message=="D3"){
+    // * * * _
+  }
+  else if (Message=="D4"){
+    // * * * * use # to validate code
+  }
+}
+void DataBase(){
+  while(true){
+    //animations courtes?
+    if (ReadInstantMsgFromArduino()=="DATABASED"){
+      return;
+    }
+  }
+}
--- I2C communication/UNO_Slave/UNO_Slave.ino
+++ I2C communication/UNO_Slave/UNO_Slave.ino
@@ -18,30 +18,29 @@ void setup() {
 
 void loop() {
   if (0<Serial.available()){
-      delay(TimeoutMsgResponse); //laisser un peu de temps pour que le court message arrive
-      while(Serial.available()>0){
-        char Serialdata =Serial.read();
-        StringBuilder+=String(Serialdata);
-      }
-      if (0==Serial.available()){
-        if (Serialdata=='1'){
-          Serial.print("OK");
-          Serial.print(GetCardID());
-        }
-      }
+    String StringBuilder="";
+    delay(TimeoutMsgResponse); //laisser un peu de temps pour que le court message arrive
+    while(Serial.available()>0){
+      char Serialdata =Serial.read();
+      StringBuilder+=String(Serialdata);
     }
-  /*if (0!=Serial.available()){ //Serial.isavailable() renvoie la quantité d'octets disponible 
-    char Serialdata =Serial.read();
-    if (Serialdata=='1'){
+    if (StringBuilder==String("SHINY")){
       Serial.print("OK");
       Serial.print(GetCardID());
     }
-  }*/ //ancien code le nouveau en théorie permet de récupérer plus que 1 caractère
+    else if(StringBuilder==String("SHINO")){
+      Serial.print("OK");
+      Shiny(10, 500);
+    }
+    else{
+      Serial.print("ERROR");
+    }
+  }
 }
 
 
 String GetCardID(){
-  delay(10000);
+  delay(1000);
   Shiny(100, 50);
   return "D2HJ82";
 }
