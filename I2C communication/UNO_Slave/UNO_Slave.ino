6bd6833a43cb220543dbf9d1263f69c7edaa6e58
Adsdworld <128044117+Adsdworld@users.noreply.github.com>
2023-10-23 09:20:16 +0200

    petit modif script de com

--- I2C communication/UNO_Slave/UNO_Slave.ino
+++ I2C communication/UNO_Slave/UNO_Slave.ino
@@ -1,3 +1,5 @@
+int TimeoutMsgResponse=1000;
+
 void Shiny(int nb, int ms){
   for (int i=1;i<nb;i++){
     digitalWrite(LED_BUILTIN, HIGH);
@@ -12,18 +14,29 @@ void Shiny(int nb, int ms){
 void setup() {
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
-  delay(10000);
-  Shiny(10, 100);
 }
 
 void loop() {
-  if (0!=Serial.available()){ //Serial.isavailable() renvoie la quantité d'octets disponible 
+  if (0<Serial.available()){
+      delay(TimeoutMsgResponse); //laisser un peu de temps pour que le court message arrive
+      while(Serial.available()>0){
+        char Serialdata =Serial.read();
+        StringBuilder+=String(Serialdata);
+      }
+      if (0==Serial.available()){
+        if (Serialdata=='1'){
+          Serial.print("OK");
+          Serial.print(GetCardID());
+        }
+      }
+    }
+  /*if (0!=Serial.available()){ //Serial.isavailable() renvoie la quantité d'octets disponible 
     char Serialdata =Serial.read();
     if (Serialdata=='1'){
       Serial.print("OK");
       Serial.print(GetCardID());
     }
-  }
+  }*/ //ancien code le nouveau en théorie permet de récupérer plus que 1 caractère
 }
 
 
