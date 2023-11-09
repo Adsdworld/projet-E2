107bb08aa721a907f5d6be032ac4a027ed0c62e0
Adsdworld <128044117+Adsdworld@users.noreply.github.com>
2023-11-09 12:48:33 +0100

    améliorations

--- CODE ARDUINO/Arduino_WIFI/Arduino_WIFI.ino
+++ CODE ARDUINO/Arduino_WIFI/Arduino_WIFI.ino
@@ -177,7 +177,34 @@ int ExtractFieldValue(String dataReceived, String fieldName) {
 * RFID:                                                                                                                                 *
 *   ???                                                                                                                                 *
 ****************************************************************************************************************************************/
-
+#include <SPI.h> // SPI
+#include <MFRC522.h> //RFID
+#define SS_PIN D10 //Use D before for the WIFI CARD
+#define RST_PIN D9 //Use D before for the WIFI CARD
+MFRC522 rfid(SS_PIN, RST_PIN);
+void RFIDsetup() {
+  Serial.begin(115200);
+  SPI.begin();
+  rfid.PCD_Init();
+}
+String GetId() {
+  while (!rfid.PICC_IsNewCardPresent()) {//il attends dans cette boucle, ajout de millis plus tard
+  }
+  if (!rfid.PICC_ReadCardSerial()) {
+    String id;
+    for (byte i = 0; i < 4; i++) {
+      id += String(rfid.uid.uidByte[i], HEX);
+    }
+    rfid.PICC_HaltA();     //permet d'arrêter la communication avec la carte
+    rfid.PCD_StopCrypto1();//permet d'arrêter la communication avec la carte
+    if(id==0000 || id=="0000"){
+      return GetId();
+    }else{
+      return id;
+    }
+  }
+  return GetId();
+}
 /****************************************************************************************************************************************
 * DISTRIBUTEUR:                                                                                                                         *
 *   ???                                                                                                                                 *
